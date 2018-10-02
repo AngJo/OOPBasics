bb6e4e052b408f041175e8d37e3935f0ee592de6
Jo-Ann <princessgsm@live.nl>
2018-10-02 12:13:02 +0200

    import

---
+++ Docent.cpp
@@ -0,0 +1,12 @@
+//
+// Created by User on 25-9-2018.
+//
+
+#include "Docent.h"
+
+Docent::Docent(string voorNaam, string achterNaam, string afkorting) {
+    voorNaam = voorNaam;
+    achterNaam = achterNaam;
+    afkorting = afkorting;
+}
+
---
+++ Docent.h
@@ -0,0 +1,18 @@
+//
+// Created by User on 25-9-2018.
+//
+
+#ifndef OOPBASICS_DOCENT_H
+#define OOPBASICS_DOCENT_H
+
+
+class Docent {
+private:
+    string voorNaam, achterNaam, afkorting;
+
+public:
+    Docent(string voorNaam, string achterNaam, string afkorting);
+};
+
+
+#endif //OOPBASICS_DOCENT_H
