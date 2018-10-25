f34996aa8f3ce51741164941b547ba1d00812890
Jo-Ann <princessgsm@live.nl>
2018-10-25 11:50:11 +0200

    Add files via upload
    Opdracht 4, Deel 2 (nog niet 100% werkend)

--- Opdracht4/main.cpp
+++ Opdracht4/main.cpp
@@ -1,31 +1,64 @@
 #include <iostream>
 #include <fstream>
+#include "IOCommands.h"
+
 using namespace std;
 
 int main() {
-    char data;
-    char c1;
-    int min = 1;
+    //char c1;
+    IOCommands coms = IOCommands();
+    char line1[100];
+    char line2[100];
+    int lengthFile1;
+    int lengthFile2;
+    int n1 = 0;
+    int n2 = 0;
 
-    ifstream infile;
-    infile.open("afile.dat");
+    ifstream infile1;
+    ifstream infile2;
+    ofstream outputFile;
 
+    lengthFile1 = infile1.tellg();
+    lengthFile2 = infile2.tellg();
 
-    infile.seekg(0, infile.end);
-    int length = infile.tellg();
-    infile.seekg(0, ios::beg);
+    infile1.open("Text1.txt");
+    infile2.open("C:\\Users\\User\\CLionProjects\\OOPBasics\\Opdracht4\\Text2.txt");
+    outputFile.open("JoAnnOutputText.txt");
 
-    for(int n = 0; n < (length - 2); n += 1){
-        infile.ignore((length-2)-min);
-        c1 = infile.get();
-        cout << c1 << "This it the " << (length-2)-n << " char" << endl;
-        min ++;
+    if (!infile1 || !infile2 || !outputFile){
+        cout << "Probleem bij openen file" << endl;
+        exit(1);
     }
 
+    //Programma schrijft eerste regel uit beide bestanden maar pakt nog niet de rest
+    if (!infile1.eof() || !infile2.eof()){
+
+    while (infile1.peek() != '\n'){
+        infile1 >> line1;
+        if (n1 >= 1){outputFile << " " << line1;}
+        else {outputFile << line1; n1++;}
+    }
+    outputFile << endl;
+
+    while (infile2.peek() != '\n'){
+        infile2 >> line2;
+        if (n2 >= 1){outputFile << " " << line2;}
+        else {outputFile << line2; n2++;}
+    }
+    outputFile  << endl;}
+
+
+    infile1.close();
+    infile2.close();
+    outputFile.close();
+
+
+    /*ifstream infile1;
+    infile1.open("afile.dat");
+    coms.readFileBackwards(infile1);*/
 
 
 
-    infile.close();
 
     return 0;
 }
